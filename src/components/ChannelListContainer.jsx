60f25b2fc033f6d3bbf3ba5b58ca717ff388ffd4
Josephmar Diaz <josephmar@diaz.com>
2022-07-06 16:02:13 -0400

    fixed styles

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -126,22 +126,22 @@ const ChannelListContainer = ({ setCreateType, setIsCreating, setIsEditing }) =>
               setIsEditing={setIsEditing} 
             />
           </div>
-
           <div className="channel-list__container-responsive"
-              style={{ left: toggleContainer ? "0%" : "-89%", backgroundColor: "#005fff"}}
-          >
-              <div className="channel-list__container-toggle" onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}>
-              </div>
-              <ChannelListContent 
-              setIsCreating={setIsCreating} 
-              setCreateType={setCreateType} 
-              setIsEditing={setIsEditing}
-              setToggleContainer={setToggleContainer}
-            />
-          </div>
+                style={{ left: toggleContainer ? "0%" : "-89%", backgroundColor: "#005fff"}}
+            >
+                <div className="channel-list__container-toggle" onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}>
+                </div>
+                <ChannelListContent 
+                setIsCreating={setIsCreating} 
+                setCreateType={setCreateType} 
+                setIsEditing={setIsEditing}
+                setToggleContainer={setToggleContainer}
+              />
+            </div>
       </>
   )
 
 }
+
 
 export default ChannelListContainer;
